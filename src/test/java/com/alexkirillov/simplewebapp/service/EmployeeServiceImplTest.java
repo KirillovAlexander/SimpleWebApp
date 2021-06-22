247ce44d4a87329591b3b79cafa626960fe12cc8
Kirillov Alexander <kirillovbrest@gmail.com>
2021-06-22 15:49:59 +0300

    Some tests added

---
+++ src/test/java/com/alexkirillov/simplewebapp/service/EmployeeServiceImplTest.java
@@ -0,0 +1,31 @@
+package com.alexkirillov.simplewebapp.service;
+
+import com.alexkirillov.simplewebapp.dao.EmployeeDAO;
+import com.alexkirillov.simplewebapp.exception.NoSuchEmployeeException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeServiceImplTest {
+
+    private static EmployeeServiceImpl employeeService;
+
+    @BeforeAll
+    public static void init() {
+        EmployeeDAO employeeDAOMock = Mockito.mock(EmployeeDAO.class);
+        Mockito.when(employeeDAOMock.getEmployee(0))
+                .thenReturn(Optional.empty());
+        employeeService = new EmployeeServiceImpl(employeeDAOMock);
+    }
+
+    @Test
+    void getEmployee() {
+        Throwable thrown = assertThrows(NoSuchEmployeeException.class, () -> employeeService.getEmployee(0));
+        assertNotNull(thrown.getMessage());
+    }
+}
