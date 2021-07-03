cc059a1797162255135f593e3e63766c33486e69
Kirillov Alexander <kirillovbrest@gmail.com>
2021-07-03 20:19:37 +0300

    slf4j added

--- src/main/java/com/alexkirillov/simplewebapp/exception/handling/EmployeeServiceGlobalExceptionHandler.java
+++ src/main/java/com/alexkirillov/simplewebapp/exception/handling/EmployeeServiceGlobalExceptionHandler.java
@@ -3,7 +3,8 @@ package com.alexkirillov.simplewebapp.exception.handling;
 import com.alexkirillov.simplewebapp.dto.MessageDTO;
 import com.alexkirillov.simplewebapp.exception.EmployeeServiceException;
 import com.alexkirillov.simplewebapp.exception.EmployeeServiceNotFoundException;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,35 +15,28 @@ import java.util.Arrays;
 @RestControllerAdvice
 public class EmployeeServiceGlobalExceptionHandler {
 
-    private final static Logger logger = Logger.getLogger(EmployeeServiceGlobalExceptionHandler.class);
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeServiceGlobalExceptionHandler.class);
+
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(EmployeeServiceNotFoundException.class)
     public MessageDTO employeeServiceNotFoundException(EmployeeServiceNotFoundException e) {
-        logger.error("employeeServiceNotFoundException: " + getStackTraceMessage(e));
+        logger.error("employeeServiceNotFoundException: ", e);
         return new MessageDTO(e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(EmployeeServiceException.class)
     public MessageDTO employeeServiceException(EmployeeServiceException e) {
-        logger.error("employeeServiceException: " + getStackTraceMessage(e));
+        logger.error("employeeServiceException: ", e);
         return new MessageDTO(e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Throwable.class)
     public MessageDTO throwableException(Throwable e) {
-        logger.error("throwableException: " + getStackTraceMessage(e));
+        logger.error("throwableException: ", e);
         return new MessageDTO(e.getMessage());
     }
 
-    private String getStackTraceMessage(Throwable exception) {
-        StringBuilder sb = new StringBuilder();
-        Arrays.stream(exception.getStackTrace()).forEachOrdered((item) -> {
-            sb.append("\n");
-            sb.append(item);
-        });
-        return sb.toString();
-    }
 }
--- src/main/java/com/alexkirillov/simplewebapp/rest/EmployeeController.java
+++ src/main/java/com/alexkirillov/simplewebapp/rest/EmployeeController.java
@@ -3,6 +3,9 @@ package com.alexkirillov.simplewebapp.rest;
 import com.alexkirillov.simplewebapp.dto.Employee;
 import com.alexkirillov.simplewebapp.dto.MessageDTO;
 import com.alexkirillov.simplewebapp.service.EmployeeService;
+import com.alexkirillov.simplewebapp.service.EmployeeServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +19,7 @@ import java.util.List;
 @RequestMapping("/api/employees")
 public class EmployeeController {
     private final EmployeeService employeeService;
+    private final static Logger logger = LoggerFactory.getLogger(EmployeeController.class);
 
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
@@ -23,26 +27,31 @@ public class EmployeeController {
 
     @GetMapping
     public List<Employee> showAllEmployees() {
+        logger.debug("showAllEmployees()");
         return employeeService.getAllEmployees();
     }
 
     @GetMapping("/{id}")
     public Employee getEmployee(@NotNull @Positive @PathVariable long id) {
+        logger.debug("getEmployee(id) where id = {}", id);
         return employeeService.getEmployee(id);
     }
 
     @PostMapping
     public Employee addEmployee(@Valid @RequestBody Employee employee) {
+        logger.debug("addEmployee(employee) where employee = {}", employee);
         return employeeService.addEmployee(employee);
     }
 
     @PutMapping("/{id}")
     public Employee updateEmployee(@NotNull @Positive @PathVariable long id, @RequestBody Employee employee) {
+        logger.debug("updateEmployee(id, employee) where id = {} and employee = {}", id, employee);
         return employeeService.updateEmployee(id, employee);
     }
 
     @DeleteMapping("/{id}")
     public MessageDTO deleteEmployee(@NotNull @Positive @PathVariable long id) {
+        logger.debug("deleteEmployee(id) where id = {}", id);
         employeeService.deleteEmployee(id);
         return new MessageDTO("Employee with id = " + id + " was deleted");
     }
